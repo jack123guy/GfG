d45b3fb0f07b38d8a9362c82d5b57c3971ac4915
jack123guy <jack123guy@gmail.com>
2017-01-20 08:51:01 +0800

    Done 2017/01/20

--- GeeksforGeeks/src/GoG_Quiz/IslandPerimeter.java
+++ GeeksforGeeks/src/GoG_Quiz/IslandPerimeter.java
@@ -1,5 +1,37 @@
 package GoG_Quiz;
 
-public class IslandPerimeter {
+//https://leetcode.com/problems/island-perimeter/
+import static java.lang.System.out;
 
+public class IslandPerimeter {
+	static int[][] island = { { 0, 1, 0, 0 }, { 1, 1, 1, 0 }, { 0, 1, 0, 0 }, { 1, 1, 0, 0 } };
+
+	public static void main(String[] args) {
+		out.println(islandPerimeter(island));
+	}
+
+	public static int islandPerimeter(int[][] grid) {
+		int peri = 0;
+		int row = 0, col = 0;
+		while (row < grid.length) {
+
+			if (grid[row][col] == 1) {
+				peri += 4;
+
+				if (col + 1 != grid[row].length && grid[row][col + 1] == 1) {
+					peri -= 2;
+				}
+				if (row + 1 != grid.length && grid[row + 1][col] == 1) {
+					peri -= 2;
+				}
+			}
+			if (col == grid[row].length - 1) {
+				row++;
+				col = 0;
+			} else {
+				col++;
+			}
+		}
+		return peri;
+	}
 }
